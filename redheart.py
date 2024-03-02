67b2964da05704b19781404adb05f49d2c814eed
bhoomika <109898065+bbhoom@users.noreply.github.com>
2024-03-02 22:20:36 +0530

    Update redheart.py

--- redheart.py
+++ redheart.py
@@ -9,7 +9,7 @@ def heartb(k):
     math.cos(4*k)
 speed(1000)
 bgcolor("black")
-for i in range(75):
+for i in range(225):
     goto(hearta(i)*20,heartb(i)*20)
     for j in range(5):
         color("red")
